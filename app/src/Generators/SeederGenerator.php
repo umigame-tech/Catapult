10526faee9123fe37209ccb42a4c55d0aa3f8f94
newnakashima <newnakashima2014@gmail.com>
2023-07-05 17:31:32 +0900

    feat: seederのテンプレート分離

--- app/src/Generators/SeederGenerator.php
+++ app/src/Generators/SeederGenerator.php
@@ -1,45 +1,24 @@
 <?php
 
 namespace UmigameTech\Catapult\Generators;
+use UmigameTech\Catapult\Templates\Renderer;
 
 class SeederGenerator extends Generator
 {
 
     public function generateDatabaseSeeder($entities)
     {
-        $seedersList = array_map(
+        $seeders = array_map(
             function ($entity) {
-                $entityName = implode('', array_map(
-                    fn ($word) => ucfirst($word),
-                    explode('_', $entity['name'])
-                ));
-
-                return "\$this->call({$entityName}Seeder::class);";
+                return ModelGenerator::modelName($entity) . 'Seeder';
             },
             $entities
         );
-        $seeders = implode("\n{$this->indents(2)}", $seedersList);
 
-        $seeder = <<<EOF
-<?php
-
-namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        {$seeders}
-    }
-}
-
-EOF;
+        $renderer = Renderer::getInstance();
+        $seeder = $renderer->render('seeders/database.twig', [
+            'seeders' => $seeders,
+        ]);
 
         $projectPath = $this->projectPath();
         $seederPath = "{$projectPath}" . '/database/seeders/DatabaseSeeder.php';
@@ -49,34 +28,16 @@ EOF;
 
     public function generate($entity)
     {
-        $modelName = implode('', array_map(
-            fn ($word) => ucfirst($word),
-            explode('_', $entity['name'])
-        ));
+        $modelName = ModelGenerator::modelName($entity);
         $seederName = $modelName . 'Seeder';
 
-        $seeder = <<<EOF
-<?php
+        $renderer = Renderer::getInstance();
+        $seeder = $renderer->render('seeders/seeder.twig', [
+            'modelName' => $modelName,
+            'seederName' => $seederName,
+            'entity' => $entity,
+        ]);
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use App\Models\\{$modelName};
-
-class {$seederName} extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        {$modelName}::factory()->count(10)->create();
-    }
-}
-
-EOF;
-    
         $projectPath = $this->projectPath();
         $seederPath = "{$projectPath}" . '/database/seeders/' . $seederName . '.php';
         // 既にファイルがある場合は削除してから生成する
@@ -85,5 +46,5 @@ EOF;
         }
 
         file_put_contents($seederPath, $seeder);
-    }     
+    }
 }
