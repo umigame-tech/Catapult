648d3086d5b1de077ec7f3313f09fa00019bf6a1
newnakashima <newnakashima2014@gmail.com>
2023-07-05 18:11:16 +0900

    feat: showページを表示できるようになった

--- app/src/Generators/ViewGenerator.php
+++ app/src/Generators/ViewGenerator.php
@@ -7,24 +7,27 @@ use UmigameTech\Catapult\Templates\Renderer;
 
 class ViewGenerator extends Generator
 {
+    private $projectPath = '';
     // view CRUD用のBladeテンプレート
     public function generate($entity)
     {
-        $this->generateIndexView($entity);
+        $projectPath = $this->projectPath();
+        $this->projectPath = $projectPath;
 
-        // TODO: index以外のビューテンプレート生成
+        // 前回のディレクトリが残っている場合は削除する
+        if (file_exists($this->projectPath . '/resources/views/' . $entity['name'])) {
+            exec("rm -rf {$this->projectPath}/resources/views/{$entity['name']}");
+        }
+
+        mkdir($this->projectPath . '/resources/views/' . $entity['name'], 0755, true);
+
+        $this->generateIndexView($entity);
+        $this->generateShowView($entity);
     }
 
     private function generateIndexView($entity)
     {
-        $projectPath = $this->projectPath();
-        // 前回のディレクトリが残っている場合は削除する
-        if (file_exists($projectPath . '/resources/views/' . $entity['name'])) {
-            exec("rm -rf {$projectPath}/resources/views/{$entity['name']}");
-        }
-
-        mkdir($projectPath . '/resources/views/' . $entity['name'], 0755, true);
-        $viewPath = $projectPath . '/resources/views/' . $entity['name'] . '/index.blade.php';
+        $viewPath = $this->projectPath . '/resources/views/' . $entity['name'] . '/index.blade.php';
 
         $modelName = ModelGenerator::modelName($entity);
         $camelCase = lcfirst($modelName);
@@ -37,6 +40,18 @@ class ViewGenerator extends Generator
             'entity' => $entity,
             'plural' => $plural,
             'camelCase' => $camelCase,
+        ]);
+
+        file_put_contents($viewPath, $view);
+    }
+
+    private function generateShowView($entity)
+    {
+        $viewPath = $this->projectPath . '/resources/views/' . $entity['name'] . '/show.blade.php';
+
+        $renderer = Renderer::getInstance();
+        $view = $renderer->render('views/show.blade.php.twig', [
+            'entity' => $entity,
         ]);
 
         file_put_contents($viewPath, $view);
